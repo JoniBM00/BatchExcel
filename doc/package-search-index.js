6aea51adb5dea3699a665abded160c2a7051a0f3
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-07 16:46:12 +0200

    fix: :ambulance: Se me olvido el javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.viewnext.batchexcel"},{"l":"com.viewnext.batchexcel.configuration"},{"l":"com.viewnext.batchexcel.listener"},{"l":"com.viewnext.batchexcel.model"},{"l":"com.viewnext.batchexcel.reader"},{"l":"com.viewnext.batchexcel.writer"}];updateSearchResults();
